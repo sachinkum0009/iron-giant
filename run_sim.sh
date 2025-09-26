49c8e18dccfa80fd6e4e6da2372534ee8e26fbec
Sachin Kumar <sachinkum123567@gmail.com>
2025-09-26 20:08:54 +0200

    usd file updated with command to run isaac sim with correct usd

--- run_sim.sh
+++ run_sim.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/bash
 
-export DISPLAY=:1
-
-/home/asus/backup/zzzzz/isaac/native/isaacsim_standalone/python.sh /home/asus/backup/zzzzz/isaac/revel_hackathon/robots/scripts/main_world.py /home/asus/backup/zzzzz/isaac/revel_hackathon/CRX_10iA_L_gripper.usd
-# /home/asus/backup/zzzzz/isaac/native/isaacsim_standalone/python.sh /home/asus/backup/zzzzz/isaac/revel_hackathon/robots/scripts/main_world.py /home/asus/backup/zzzzz/isaac/revel_hackathon/robots/library/Fanuc/CRX_10iA_L_v1/CRX_10iA_L/CRX_10iA_L.usd
+python.sh scripts/main_world.py /home/asus/backup/zzzzz/isaac/revel_hackathon/CRX_10iA_L_gripper.usd
